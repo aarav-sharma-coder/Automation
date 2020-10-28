8ff06d5e1e4147f6283f08d208d5842e5412843f
aarav-sharma-coder <62800033+aarav-sharma-coder@users.noreply.github.com>
2020-10-28 14:19:45 +0530

    Add files via upload

---
+++ automate.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import time
+
+def backup():
+    source = input("Enter the DRIVE to be backed up: ")
+    destination = "F:"
+
+    source = source + '/'
+    destination = destination + '/'
+
+    list_of_files = os.listdir(source)
+
+    seconds = time.time() - (30 * 24 * 60 * 60)
+
+
+    if (seconds >= get_file_or_folder_age(source)):
+
+        with open(source, 'rb') as f:  
+            shutil.copy(source, destination)
+
+
+def get_file_or_folder_age(path):
+
+	ctime = os.stat(path).st_ctime
+
+	return ctime
+
+
+backup()
